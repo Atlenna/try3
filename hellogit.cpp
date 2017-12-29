2f04f43d591f37e29dbe12c45cd1aa1f8c42daa1
fareden <fareden@gmail.com>
2017-12-29 09:18:27 -0600

    Added a function
    Added a function stub

--- hellogit.cpp
+++ hellogit.cpp
@@ -2,6 +2,13 @@
 #include <iostream>
 using namespace std;
 
+void aFunction(int param);
+
 int main(int argc, char** argv) {
-    std::cout << "This simply writes a line";
+    std::cout << "This simply writes a line" << std::endl;
+    aFunction(argc);
+}
+
+void aFunction(int param) {
+    std::cout << "The param is: " << param << std::endl;
 }
